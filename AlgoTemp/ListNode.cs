938102bc94da788deadaf9a5821051733cf5b3d7
FayzullaevJavlonbek <javlonbek.fayzullaev@webase.uz>
2025-09-27 11:14:50 +0500

    Add project files.

---
+++ AlgoTemp/ListNode.cs
@@ -0,0 +1,13 @@
+﻿namespace AlgoTemp
+{
+    public class ListNode
+    {
+        public int Value { get; private set; }
+        public ListNode Next { get; internal set; }
+        public ListNode(int value = 0, ListNode next = null!)
+        {
+            Value = value;
+            Next = next;
+        }
+    }
+}
---
+++ AlgoTemp/TreeNode.cs
@@ -0,0 +1,44 @@
+﻿namespace AlgoTemp
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static class Helper
+    {
+        public static void DFS(TreeNode root)
+        {
+            if (root == null) return;
+            Console.WriteLine(root.val);
+            DFS(root.left);
+            DFS(root.right);
+        }
+
+        public static void BFS(TreeNode root)
+        {
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+
+            queue.Enqueue(root);
+
+            while (queue.Any())
+            {
+                TreeNode node = queue.Dequeue();
+
+                if (node == null) continue;
+                Console.WriteLine(node.val);
+
+                queue.Enqueue(node.left);
+                queue.Enqueue(node.right);
+            }
+        }
+    }
+}
